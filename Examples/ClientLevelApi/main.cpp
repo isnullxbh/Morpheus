f4f47ce5f596c1649db1c7087a6693e00139f45d
Oleg E. Vorobiov <isnullxbh@gmail.com>
2024-05-27 23:59:34 +0700

    Refactoring: ClientLevelApi

--- Examples/ClientLevelApi/main.cpp
+++ Examples/ClientLevelApi/main.cpp
@@ -10,85 +10,41 @@ import Morpheus.PostgreSql.Client;
 import Morpheus.SQLite.Client;
 import Morpheus.Odbc.Client;
 
-void postgreSql();
-void sqlite();
-void odbc();
+auto test(Morpheus::Sql::Client& client, std::string_view connection_str) -> void;
 
 int main()
 {
-    postgreSql();
-    sqlite();
-    odbc();
+    {
+        Morpheus::PostgreSql::Client client {};
+        test(client, "postgresql://localhost:5432/testdb");
+    }
+
+    {
+        Morpheus::SQLite::Client client {};
+        test(client, "file:///Users/isnullxbh/testdb");
+    }
+
+    {
+        auto create_client = Morpheus::Odbc::Client::create();
+        if (!create_client)
+        {
+            std::cerr << "An error occurred: " << create_client.error().message() << std::endl;
+        }
+        else
+        {
+            auto client = std::move(create_client).value();
+            test(client, "Driver={PostgreSqlOdbc};Database=testdb;Port=5432;");
+        }
+    }
+
     return 0;
 }
 
-void postgreSql()
+auto test(Morpheus::Sql::Client& client, std::string_view connection_str) -> void
 {
-    using namespace Morpheus::PostgreSql;
     using Morpheus::Uri;
 
-    Client client {};
-    auto try_connect = client.connect(Uri("postgresql://localhost:5432/testdb"));
-    if (!try_connect)
-    {
-        std::cerr << "An error occurred: " << try_connect.error().message() << std::endl;
-        return;
-    }
-
-    auto connection = std::move(try_connect).value();
-
-    auto try_execute = connection->execute("SELECT * FROM user");
-    if (!try_execute)
-    {
-        std::cerr << "An error occurred: " << try_execute.error().message() << std::endl;
-        return;
-    }
-
-    auto result_set = std::move(try_execute).value();
-    std::cout << "Result set size: " << result_set->size() << std::endl;
-}
-
-void sqlite()
-{
-    using namespace Morpheus::SQLite;
-    using Morpheus::Uri;
-
-    Client client {};
-    auto try_connect = client.connect(Uri("file:///Users/isnullxbh/testdb"));
-    if (!try_connect)
-    {
-        std::cerr << "An error occurred: " << try_connect.error().message() << std::endl;
-        return;
-    }
-
-    auto connection = std::move(try_connect).value();
-
-    auto try_execute = connection->execute("SELECT * FROM user");
-    if (!try_execute)
-    {
-        std::cerr << "An error occurred: " << try_execute.error().message() << std::endl;
-        return;
-    }
-
-    auto result_set = std::move(try_execute).value();
-    std::cout << "Result set size: " << result_set->size() << std::endl;
-}
-
-void odbc()
-{
-    using namespace Morpheus::Odbc;
-    using Morpheus::Uri;
-
-    auto create_client = Client::create();
-    if (!create_client)
-    {
-        std::cerr << "An error occurred: " << create_client.error().message() << std::endl;
-        return;
-    }
-
-    auto client = std::move(create_client).value();
-
-    auto try_connect = client.connect(Uri("Driver={PostgreSqlOdbc};Database=testdb;Port=5432;"));
+    auto try_connect = client.connect(Uri(connection_str));
     if (!try_connect)
     {
         std::cerr << "An error occurred: " << try_connect.error().message() << std::endl;
